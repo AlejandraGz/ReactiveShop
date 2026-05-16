3b8d31c8f9d979bd2190fd3fa6081525155cbf34
Alejandra Gonzalez Melendez <alejandragm929@gmail.com>
2026-05-16 13:24:24 -0500

    Listar productos #7

--- src/app/features/carrito/pages/sidebar-carrito/sidebar-carrito.ts
+++ src/app/features/carrito/pages/sidebar-carrito/sidebar-carrito.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
 import { Producto } from '../../../../models/producto.model';
 import { Carrito } from '../../services/carrito';
+import { CurrencyPipe } from '@angular/common';
+import { RouterLink } from '@angular/router';
+import { MatIcon } from "@angular/material/icon";
+
 
 @Component({
   selector: 'app-sidebar-carrito',
-  imports: [],
+  imports: [CurrencyPipe, RouterLink, MatIcon],
   templateUrl: './sidebar-carrito.html',
-  styleUrl: './sidebar-carrito.css',
+  styleUrls: [
+    './sidebar-carrito.css',
+    '../../../../../styles/_variables.css'
+  ]
 })
 export class SidebarCarrito implements OnInit {
   productos: Producto[] = [];
+  totalCarrito: number = 0;
   constructor(private carritoService: Carrito) { }
 
   ngOnInit() {
@@ -17,5 +25,13 @@ export class SidebarCarrito implements OnInit {
     this.carritoService.productos.subscribe(p => {
       this.productos = p;
     });
+  }
+
+  slugify(text: string): string {
+    return text
+      .toLowerCase()
+      .replace(/ /g, '-')
+      .normalize('NFD')
+      .replace(/[\u0300-\u036f]/g, '');
   }
 }
