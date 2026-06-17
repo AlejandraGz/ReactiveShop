65b265efc60cbc2e9bb0645e577ee5010be7c2b2
Alejandra Gonzalez Melendez <alejandragm929@gmail.com>
2026-06-17 17:17:01 -0500

    Sincronizar carrito globalmente  #12

--- src/app/layouts/navbar/navbar.ts
+++ src/app/layouts/navbar/navbar.ts
@@ -2,14 +2,23 @@ import { Component } from '@angular/core';
 import { MatToolbar } from '@angular/material/toolbar';
 import { MatIcon } from '@angular/material/icon';
 import { RouterLink } from '@angular/router';
+import { Carrito } from '../../features/carrito/services/carrito';
+import { AsyncPipe } from '@angular/common';
+import { MatBadge } from '@angular/material/badge';
 
 @Component({
   selector: 'app-navbar',
-  imports: [MatToolbar, MatIcon, RouterLink],
+  imports: [MatToolbar, MatIcon, MatBadge, RouterLink, AsyncPipe],
   templateUrl: './navbar.html',
   styleUrls: [
     './navbar.css',
     '../../../styles/_variables.css'
   ]
 })
-export class Navbar {}
+export class Navbar {
+  cantidadProductos$;
+  
+  constructor(private carritoService: Carrito) {
+    this.cantidadProductos$ = carritoService.cantidadProductos$
+  }
+}
