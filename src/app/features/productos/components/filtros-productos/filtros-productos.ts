4f061f42b2995a3128df629815356deb16f00de4
Alejandra Gonzalez Melendez <alejandragm929@gmail.com>
2026-06-17 16:46:30 -0500

    Implementar búsqueda con debounce  #11

--- src/app/features/productos/components/filtros-productos/filtros-productos.ts
+++ src/app/features/productos/components/filtros-productos/filtros-productos.ts
@@ -1,19 +1,20 @@
-import { Component, OnInit, Input, Output, EventEmitter  } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { Categoria } from '../../../../models/categoria.model';
 import { Data } from '../../../../core/services/data';
-import { Observable } from 'rxjs';
+import { combineLatest, debounce, debounceTime, distinctUntilChanged, map, Observable, startWith } from 'rxjs';
 import { CommonModule } from '@angular/common';
 import { Productos } from '../../services/productos';
+import { ReactiveFormsModule, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-filtros-productos',
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './filtros-productos.html',
   styleUrls: [
     './filtros-productos.css',
     '../../../../../styles/_variables.css']
 })
-export class FiltrosProductos implements OnInit{
+export class FiltrosProductos implements OnInit {
   @Input() mostrarFiltroCategoria = true;
   @Input() marcas: string[] | null = [];
   @Output() producto = new EventEmitter<string>();
@@ -23,22 +24,31 @@ export class FiltrosProductos implements OnInit{
   @Output() precioMax = new EventEmitter<number>();
 
   categorias$!: Observable<Categoria[]>
+  productos$;
 
-  constructor(private dataService: Data, private productoService: Productos){}
+  buscadorControl = new FormControl('');
+  constructor(private dataService: Data, private productoService: Productos) {
+
+    this.productos$ = productoService.getProductos();
+
+  }
 
   ngOnInit(): void {
     this.categorias$ = this.dataService.getCategorias();
-  }
-  buscarProducto(event: Event) {
-    const input = event.target as HTMLInputElement;
-    this.producto.emit(input.value);
-  }
 
-  buscarCategoria(event: Event){
+      this.buscadorControl.valueChanges.pipe(
+        debounceTime(300),
+        distinctUntilChanged()
+      ).subscribe(texto => {
+        this.producto.emit(texto ?? '')
+      }
+    )
+  }
+  buscarCategoria(event: Event) {
     const categoriaSeleccionada = event.target as HTMLInputElement;
     this.categoria.emit(Number(categoriaSeleccionada.value));
   }
-  buscarMarca(event: Event){
+  buscarMarca(event: Event) {
     const marcaSeleccionada = event.target as HTMLInputElement;
     this.marca.emit(marcaSeleccionada.value);
   }
