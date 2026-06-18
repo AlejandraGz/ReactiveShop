22941b284693ca60028a913822ec160c0f7aa903
Alejandra Gonzalez Melendez <alejandragm929@gmail.com>
2026-06-17 19:02:09 -0500

    Implementar estados de carga (layout principal) #13

--- src/app/layouts/main-layout/main-layout.ts
+++ src/app/layouts/main-layout/main-layout.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from "@angular/router";
 import { Footer } from '../footer/footer';
 import { Navbar } from "../navbar/navbar";
+import { LoadingComponent } from "../../features/loading/loading/loading";
 
 @Component({
   selector: 'app-main-layout',
-  imports: [RouterOutlet, Footer, Navbar],
+  imports: [RouterOutlet, Footer, Navbar, LoadingComponent],
   templateUrl: './main-layout.html',
   styleUrl: './main-layout.css',
 })
