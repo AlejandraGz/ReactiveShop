ca08a415894a848639ea1a7a68928aaa0944144a
Alejandra Gonzalez Melendez <alejandragm929@gmail.com>
2026-08-01 12:25:25 -0500

    Auto-generated commit

---
+++ chunk-SNUMDLRT.js
@@ -0,0 +1,1 @@
+import{B as i,E as e,j as o,kb as a}from"./chunk-SZTKHPPS.js";var p=class t{constructor(r){this.http=r}url="assets/data.json";getProductos(){return this.http.get(this.url).pipe(o(r=>r.productos))}static \u0275fac=function(n){return new(n||t)(e(a))};static \u0275prov=i({token:t,factory:t.\u0275fac,providedIn:"root"})};export{p as a};
