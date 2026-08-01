f2e9a74a8649b00b6f6cd8c9d0823d0c52f1a8c8
Alejandra Gonzalez Melendez <alejandragm929@gmail.com>
2026-08-01 12:06:24 -0500

    style: mejorar diseño responsive del navbar #19

--- src/app/layouts/navbar/navbar.ts
+++ src/app/layouts/navbar/navbar.ts
@@ -1,14 +1,18 @@
 import { Component } from '@angular/core';
 import { MatToolbar } from '@angular/material/toolbar';
 import { MatIcon } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 import { RouterLink } from '@angular/router';
 import { Carrito } from '../../features/carrito/services/carrito';
 import { AsyncPipe } from '@angular/common';
 import { MatBadge } from '@angular/material/badge';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { map, Observable, shareReplay } from 'rxjs';
 
 @Component({
   selector: 'app-navbar',
-  imports: [MatToolbar, MatIcon, MatBadge, RouterLink, AsyncPipe],
+  standalone: true,
+  imports: [MatToolbar, MatIcon, MatButtonModule, MatBadge, RouterLink, AsyncPipe],
   templateUrl: './navbar.html',
   styleUrls: [
     './navbar.css',
@@ -17,8 +21,22 @@ import { MatBadge } from '@angular/material/badge';
 })
 export class Navbar {
   cantidadProductos$;
-  
-  constructor(private carritoService: Carrito) {
+  isHandSet$!: Observable<boolean>;
+  menuAbierto: boolean = false;
+
+  constructor(
+    private carritoService: Carrito,
+    private breakpointObserver: BreakpointObserver
+  ) {
     this.cantidadProductos$ = carritoService.cantidadProductos$
+
+    this.isHandSet$ = this.breakpointObserver
+      .observe(Breakpoints.Handset)
+      .pipe(
+        map(result => result.matches)
+      );
+  }
+  toggleMenu() {
+    this.menuAbierto = !this.menuAbierto
   }
 }
